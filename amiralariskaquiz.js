af0b4f6a4ae9138ff0880eab86925f8f14507681
Amiral Ariska <ariskaamiral@gmail.com>
2022-02-26 19:50:48 +0800

    uncomment all javascript file

--- amiralariskaquiz.js
+++ amiralariskaquiz.js
@@ -1,60 +1,60 @@
 #!/usr/bin/env node
-// var chalk = require('chalk');
-// var gradient = require('gradient-string');
-// var inquirer = require('inquirer');
-// const chalkcolor = require('chalk-color-style-form');
+var chalk = require('chalk');
+var gradient = require('gradient-string');
+var inquirer = require('inquirer');
+const chalkcolor = require('chalk-color-style-form');
 
-// var welcome = gradient('blue', 'cyan')('Welcome to Amiral Ariska Form');
-// console.log(welcome);
+var welcome = gradient('blue', 'cyan')('Welcome to Amiral Ariska Form');
+console.log(welcome);
 
-// var password = 'CodeAmiral';
-// var form = inquirer.prompt([
-//     {
-//         name: 'full name',
-//         message: 'what is your full name:',
-//         type: 'input'
-//     },
-//     {
-//         name: 'date of birth',
-//         message: 'what is your date of birth:',
-//         type: 'date',
-//         format: { day: "numeric", month: "long", year: "numeric", hour: undefined, minute: undefined },
-//     },
-//     {
-//         name: 'city',
-//         message: 'what is your city:',
-//         type: 'input'
-//     },
-//     {
-//         name: 'gender',
-//         message: 'what is your gender:',
-//         type: 'list',
-//         choices: [
-//             'Male',
-//             'Female',
-//         ]
-//     },
-//     {
-//         name: 'password',
-//         message: 'please insert password to continue:',
-//         type: 'input',
-//         default() {
-//             return password;
-//         },
-//     },
-//     {
-//         name: 'code what',
-//         message: 'what do you want to code:',
-//         type: 'input'
-//     },
-//     {
-//         name: 'confirm yes',
-//         message: 'do you want to submit',
-//         type: 'confirm'
-//     }
-// ]).then(() => {
-//     if (form) {
-//         chalkcolor.custom(`${chalkcolor.varcompleted()} ${chalk.hex('#0f0')('Your form has successfully submitted')}`)
-//     }
-// });
-// inquirer.registerPrompt("date", require("inquirer-date-prompt"));
+var password = 'CodeAmiral';
+var form = inquirer.prompt([
+    {
+        name: 'full name',
+        message: 'what is your full name:',
+        type: 'input'
+    },
+    {
+        name: 'date of birth',
+        message: 'what is your date of birth:',
+        type: 'date',
+        format: { day: "numeric", month: "long", year: "numeric", hour: undefined, minute: undefined },
+    },
+    {
+        name: 'city',
+        message: 'what is your city:',
+        type: 'input'
+    },
+    {
+        name: 'gender',
+        message: 'what is your gender:',
+        type: 'list',
+        choices: [
+            'Male',
+            'Female',
+        ]
+    },
+    {
+        name: 'password',
+        message: 'please insert password to continue:',
+        type: 'input',
+        default() {
+            return password;
+        },
+    },
+    {
+        name: 'code what',
+        message: 'what do you want to code:',
+        type: 'input'
+    },
+    {
+        name: 'confirm yes',
+        message: 'do you want to submit',
+        type: 'confirm'
+    }
+]).then(() => {
+    if (form) {
+        chalkcolor.custom(`${chalkcolor.varcompleted()} ${chalk.hex('#0f0')('Your form has successfully submitted')}`)
+    }
+});
+inquirer.registerPrompt("date", require("inquirer-date-prompt"));
